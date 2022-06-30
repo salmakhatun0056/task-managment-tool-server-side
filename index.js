0aaad927df7bea6ab80bd9b26cf0161c76caa135
salmakhatun12345 <ummesalma0056@gmail.com>
2022-06-30 22:20:01 +0600

    mongodb connected successfully

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -10,6 +11,31 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('hello');
 });
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ocpmo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const tasksCollention = client.db('taskManager').collection('task')
+
+        app.get('/get-task', async (req, res) => {
+            const tasks = await tasksCollention.find({}).toArray()
+            res.send(tasks)
+        });
+
+        app.post('/add-task', async (req, res) => {
+            const data = req.body
+            console.log(data)
+            const result = await tasksCollention.insertOne(data)
+            res.send(result)
+        })
+    } finally {
+    }
+}
+run().catch(console.dir);
 
 app.post('/add task', (req, res) => {
     const data = req.body;
